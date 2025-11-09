d8ee1e430ffa2defa9e5823bb5790d2869183925
waka <d950@protonmail.com>
2025-11-09 17:34:11 +0200

    Added linear perceptron implementation for scalars

--- computation_and_cognition/linear_perceptron.py
+++ computation_and_cognition/linear_perceptron.py
@@ -2,71 +2,66 @@
 
 import logging
 import numpy as np
+
 logger = logging.getLogger(__name__)
 
-class BinaryPerceptron(object):
+class LinearPerceptron(object):
     def __init__(self, weights: np.array):
         if weights.ndim != 2:
             raise Exception('Supports weight matrix only')
 
         self._weights = weights
 
-    def predict(self, x: np.array) -> bool:
-        prod = np.matmul(np.transpose(self._weights), x)
-        for i in prod:
-            i[0] = int(i[0] >= 0)
-
+    def predict(self, x: np.array) -> np.array:
+        prod = np.matmul(self._weights.transpose(), x)
+        logger.debug(f"Liner Perceptron Predict called for w = {self._weights}, x = {x}, wx = {prod}")
         return prod
 
     def update(self, example: np.array, example_prediction: np.array) -> bool:
-        current_prediction = self.predict(example)
-        if np.array_equal(current_prediction, example_prediction):
-            return True
+        # example is of the form of matrix such that each column is an example vector
+        # example_prediction is a matrix such that each column is a desired result
+        P = len(example)
+        C = (1 / P) * np.matmult(example, example.transpose())
+        u = (1 / P) * np.matmult(example.transpose(), y)
+        a = (1 / (2 * P)) * np.matmult(y.transpose(), y)
+        w = np.matmult(np.linalg.inv(C), u)
+        self._weights = w
 
-        a = np.diag(np.transpose(example_prediction - current_prediction)[0])
-        b = np.tile(example, (1, len(example_prediction)))
-        added_value = np.matmul(b, a)
-        self._weights += added_value
-
-class BinaryPerceptronTrainer(object):
+class LinearPerceptronTrainer(object):
     def __init__(self):
         pass
 
-    def fit(self, examples: list[np.array], example_predictions: list[np.array]):
-        initial_weights = np.ones((len(examples[0]), len(example_predictions[0])))
-        bp = BinaryPerceptron(initial_weights)
-        keep_iterating = True
-        while keep_iterating:
-            logger.debug("Starting new training iteration")
-            keep_iterating = False
-            for example_input, example_prediction in zip(examples, example_predictions):
-                if not bp.update(example_input, example_prediction):
-                    keep_iterating = True
+    def fit(self, examples: list[np.array], example_predictions: list[float]):
+        lp = LinearPerceptron(initial_weights)
+        X = np.array(examples).transpose()
+        y = np.array(number).transpose()
+        lp.update(X, y)
+        return lp
 
-        return bp
-
-def tests_run(bp: BinaryPerceptron, tests):
+def tests_run(perceptron: LinearPerceptron, tests):
     for vector, classification in tests:
-        actual = bp.predict(vector)
+        actual = perceptron.predict(vector)
         if np.array_equal(classification, actual):
             print(f"{np.transpose(vector)} was classified correctly")
         else:
-            print(f"Vector: {np.transpose(vector)}, Correct: {classification}, Actual: {binary_perceptron.predict(vector)}")
+            print(f"Vector: {np.transpose(vector)}, Correct: {classification}, Actual: {perceptron.predict(vector)}")
+
+def colvec(*values: list[float]) -> np.array:
+    return np.array([values]).transpose()
 
 def tests():
     print(" --- BEGIN Single Neuron Manual Test Section ---")
-    binary_perceptron = BinaryPerceptron(np.array([[-1], [1]]))
-    print("weights:\n", binary_perceptron._weights)
+    perceptron = LinearPerceptron(np.array([[2], [3]]))
+    print("weights:\n", perceptron._weights)
     tests = [
-            (np.transpose([[2, 1]]), np.transpose([[0]])),
-            (np.transpose([[-1, 1]]), np.transpose([[1]])),
-            (np.transpose([[-2, -1]]), np.transpose([[1]])),
-            (np.transpose([[-1, -2]]), np.transpose([[0]])),
-            (np.transpose([[1, -1]]), np.transpose([[0]]))]
-    tests_run(binary_perceptron, tests)
+            (colvec(1, 0), colvec(2)),
+            (colvec(0, 2), colvec(6)),
+            (colvec(1, -1), colvec(-1)) ]
+    tests_run(perceptron, tests)
+    return
     print("\n --- BEGIN Single Neuron Train Section ---")
     binary_perceptron_factory = BinaryPerceptronTrainer()
-    binary_perceptron = binary_perceptron_factory.fit(
+    perceptron = binary_perceptron_factory.fit(
             [
                 np.transpose([[2, 1]]),
                 np.transpose([[-1, -2]]),
@@ -75,7 +70,7 @@ def tests():
                 np.transpose([[0]]),
                 np.transpose([[0]]),
                 np.transpose([[0]])])
-    print("weights:\n", binary_perceptron._weights)
+    print("weights:\n", perceptron._weights)
     print("\n --- BEGIN Single Neuron Trained Test Section ---")
     tests = [
             (np.transpose([[2, 1]]), np.transpose([[0]])),
@@ -83,20 +78,20 @@ def tests():
             (np.transpose([[-2, -1]]), np.transpose([[1]])),
             (np.transpose([[-1, -2]]), np.transpose([[0]])),
             (np.transpose([[1, -1]]), np.transpose([[0]]))]
-    tests_run(binary_perceptron, tests)
+    tests_run(perceptron, tests)
     print("\n --- BEGIN Manual Test Section ---")
-    binary_perceptron = BinaryPerceptron(np.array([[-1, 0], [1, 1]]))
-    print("weights:\n", binary_perceptron._weights)
+    perceptron = LinearPerceptron(np.array([[-1, 0], [1, 1]]))
+    print("weights:\n", perceptron._weights)
     tests = [
             (np.transpose([[2, 1]]), np.transpose([[0, 1]])),
             (np.transpose([[-1, 1]]), np.transpose([[1, 1]])),
             (np.transpose([[-2, -1]]), np.transpose([[1, 0]])),
             (np.transpose([[-1, -2]]), np.transpose([[0, 0]])),
             (np.transpose([[1, -1]]), np.transpose([[0, 0]]))]
-    tests_run(binary_perceptron, tests)
+    tests_run(perceptron, tests)
     print("\n --- BEGIN Train Section ---")
     binary_perceptron_factory = BinaryPerceptronTrainer()
-    binary_perceptron = binary_perceptron_factory.fit(
+    perceptron = binary_perceptron_factory.fit(
             [
                 np.transpose([[2, 1]]),
                 np.transpose([[-1, -2]]),
@@ -105,7 +100,7 @@ def tests():
                 np.transpose([[0, 1]]),
                 np.transpose([[0, 0]]),
                 np.transpose([[0, 0]])])
-    print("weights:\n", binary_perceptron._weights)
+    print("weights:\n", perceptron._weights)
     print("\n --- BEGIN Trained Test Section ---")
     tests = [
             (np.transpose([[2, 1]]), np.transpose([[0, 1]])),
@@ -113,11 +108,11 @@ def tests():
             (np.transpose([[-2, -1]]), np.transpose([[1, 0]])),
             (np.transpose([[-1, -2]]), np.transpose([[0, 0]])),
             (np.transpose([[1, -1]]), np.transpose([[0, 0]]))]
-    tests_run(binary_perceptron, tests)
+    tests_run(perceptron, tests)
 
 if __name__ == "__main__":
     logging.basicConfig(
-            level = logging.INFO,
+            level = logging.DEBUG,
             handlers = [
                 logging.StreamHandler()
             ]
