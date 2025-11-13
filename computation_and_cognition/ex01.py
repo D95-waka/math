cca96f0d82908857daf04521f8941d4a53738880
waka <d950@protonmail.com>
2025-11-14 00:08:48 +0200

    More fixes for the first exercise

--- computation_and_cognition/ex01.py
+++ computation_and_cognition/ex01.py
@@ -7,7 +7,7 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 # Example usage: question_1([[1, 0], [0, 1]], [1, 0]) -> [1, -1]
-def question_1(examples: list[list[float]], predictions: list[float]) -> np.array:
+def question_1(examples: np.ndarray, predictions: list[float]) -> np.ndarray:
     bpt = BinaryPerceptronTrainer()
     bp = bpt.fit([np.transpose([x]) for x in examples], [[[x]] for x in predictions])
     return bp._weights
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     ax.scatter([x for x, y in mat if x > y], [y for x, y in mat if x > y], color = "blue")
     ax.scatter([x for x, y in mat if x <= y], [y for x, y in mat if x <= y], color = "red")
     ax.quiver(weights[0][0], weights[1][0], color = 'green')
-    plt.savefig('bin/out1-3.jpg')
+    plt.savefig('bin/out1_3.jpg')
 
     # Question 4:
     optimal_weights = np.array([[1], [-1]])
@@ -44,17 +44,17 @@ if __name__ == "__main__":
     M = 100
     data = []
     for P in [5, 20, 30, 50, 100, 150, 200, 500]:
-        sum = 0
+        sum = np.array([[0.0]])
         for i in range(M):
             mat, preds = question_2(P, 2)
             weights = question_1(mat, preds)
-            sum += (np.matmul(weights.transpose(), optimal_weights) / (np.linalg.norm(weights) * K))[0][0]
+            sum += np.rad2deg(np.arccos((weights.transpose() @ optimal_weights) / (np.linalg.norm(weights) * K)))
 
         sum /= M
-        data.append([P, sum])
+        data.append([P, sum[0][0]])
 
     data = np.transpose(data)
     fig, ax = plt.subplots(layout='constrained')
     bars = ax.bar([f"P={x}" for x in data[0]], data[1])
     ax.bar_label(bars, labels = [f"{x:.4f}" for x in data[1]], padding=3)
-    plt.savefig('bin/out1-4.jpg')
+    plt.savefig('bin/out1_4.jpg')
