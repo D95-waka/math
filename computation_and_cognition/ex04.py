1b3bb9264d1306850948937a2b4ed97c2c256331
waka <d950@protonmail.com>
2025-12-01 01:26:49 +0200

    Done with this

--- computation_and_cognition/ex04.py
+++ computation_and_cognition/ex04.py
@@ -3,6 +3,7 @@
 from pca import *
 import logging
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 
 logger = logging.getLogger(__name__)
@@ -56,32 +57,31 @@ if __name__ == "__main__":
     plt.savefig('bin/out4_0_1.jpg')
 
     # Question 1
-    data = [[float(y) for y in x[:-1].split(',')] for x in open('./wine.csv', 'r').readlines()[1:]]
+    csv = [[float(y) for y in x[:-1].split(',')] for x in open('./wine.csv', 'r').readlines()[1:]]
+    data = np.array(csv)
     logger.debug(data)
 
     # Question 2
+    patches = [
+        mpatches.Patch(color='red', label='Producer 1'),
+        mpatches.Patch(color='blue', label='Producer 2'),
+        mpatches.Patch(color='green', label='Producer 3')
+    ]
+    colors = ['red' if x[0] == 1 else 'blue' if x[0] == 2 else 'green' for x in csv]
     fig, ax = plt.subplots(layout='constrained')
-    for r in data:
-        match r[0]:
-            case 1:
-                color = 'red'
-            case 2: 
-                color = 'blue'
-            case 3:
-                color = 'green'
-            case _:
-                color = 'black'
-        ax.scatter(r[1], r[2], alpha = 0.6, s = 10, color = color)
+    ax.scatter(data[:,1], data[:,2], alpha = 0.6, s = 10, color = colors)
     plt.title('Wine DataSet partitioned by producer')
     plt.xlabel('Property 1')
     plt.ylabel('Property 2')
+    plt.legend(handles = patches)
     plt.savefig('bin/out4_2.jpg')
 
     # Question 3
-    data = np.array(data).T[1:]
-    mean = np.mean(data, axis=0)
-    std_dev = np.std(data, axis=0)
-    data = ((data - mean) / std_dev)
+    data = np.array(data)[:,1:]
+    mean = np.mean(data, axis = 0)
+    std_dev = np.std(data, axis = 0)
+    data = (data - mean) / std_dev
+    data = data.T
     pca_factory = PCAFactory()
     pca, C, values, vectors = pca_factory.inner_fit_batch(data, 2)
 
@@ -90,23 +90,29 @@ if __name__ == "__main__":
     variances = [100 * sum(values[:i + 1]) / total for i in range(len(values))]
     fig, ax = plt.subplots(layout='constrained')
     ax.scatter(range(len(variances)), variances, alpha = 0.6, s = 10)
-    plt.title('')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.title('Calculated variance per number of used dimensions')
+    plt.xlabel('Embedded Dimension')
+    plt.ylabel('error percentage')
     plt.savefig('bin/out4_5.jpg')
 
     # Question 6
     projections = np.array([pca.project(p) for p in data.T]).T
     fig, ax = plt.subplots(layout='constrained')
-    ax.scatter(projections[0], projections[1], alpha = 0.6, s = 10)
-    print("A", vectors[0])
-    for v in range(len(data.T)):
-        w = pca.project(np.array([data.T[v]]).T)
-        if w[0] > -2.2:
-            print(v)
-        #ax.scatter(w[0][0], w[1][0], alpha = 0.6, s = 10)
-
-    plt.title('')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    colors = ['red' if x[0] == 1 else 'blue' if x[0] == 2 else 'green' for x in csv]
+    ax.scatter(projections[0], projections[1], alpha = 0.6, s = 10, color = colors)
+    plt.title('Embedded space using PCA to dimension 2')
+    plt.xlabel('vector 1')
+    plt.ylabel('vector 2')
+    plt.legend(handles = patches)
     plt.savefig('bin/out4_6.jpg')
+
+    # Question 7
+    pca.components = vectors[:, (0,12)]
+    projections = np.array([pca.project(p) for p in data.T]).T
+    fig, ax = plt.subplots(layout='constrained')
+    ax.scatter(projections[0], projections[1], alpha = 0.6, s = 10, color = colors)
+    plt.title('Embedded space using first and last vectors calculated for PCA')
+    plt.xlabel('vector 1')
+    plt.ylabel('vector 13')
+    plt.legend(handles = patches)
+    plt.savefig('bin/out4_7.jpg')
