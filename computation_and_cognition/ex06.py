c68192a7042e3b9a372e47ccae1812e9ea10a61b
waka <d950@protonmail.com>
2025-12-18 22:29:41 +0200

    More of this

--- computation_and_cognition/ex06.py
+++ computation_and_cognition/ex06.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
 import logging
+import matplotlib.pyplot as plt
+import random
 
 logger = logging.getLogger(__name__)
+
+rewards = {
+    ('H', 0): 0,
+    ('H', 1): 1,
+    ('O', 0): 2,
+    ('O', 1): 0
+}
+
+def R(s, a):
+    r = rewards[(s, a)]
+    if a == 0:
+        return s, r
+
+    if s == 'O':
+        return 'H', r
+
+    return random.choices(['H', 'O'], weights = (2, 8))[0], r
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -41,3 +60,26 @@ if __name__ == "__main__":
 
     print(V)
 
+    # Question 2.5
+    V = { 'H': 0.0, 'O': 0.0 }
+    eta = 0.01
+    T = 3000
+    s = 'H'
+    data_VH = []
+    data_VO = []
+    for i in range(T):
+        data_VH.append(V['H'])
+        data_VO.append(V['O'])
+        a = random.choice([0, 1])
+        sn, r = R(s, a)
+        V[s] += eta * (r + gamma * V[sn] - V[s])
+        s = sn
+
+    print(V)
+    fig, ax = plt.subplots(layout='constrained')
+    ax.set_title("Heatmap of trained perceptron weights")
+    ax.plot(range(T), data_VO, alpha = 0.6, label = 'V(O)')
+    ax.plot(range(T), data_VH, alpha = 0.6, label = 'V(H)')
+    plt.axhline(4, color = 'black', linestyle = 'dashed', label = 'Correlation Matrix Generalization')
+    plt.show()
+    plt.savefig('bin/out6_1.jpg')
